577e96b9d88d8e4865ed3ef4a1f40c5204a560e9
manoj@ubuntu <manojkapure100@gmail.com>
2025-08-12 17:43:07 +0000

    added solution for implement stack using queue and implement queue using stack questions

---
+++ implement queue using stack.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include <stack>
+
+using namespace std;
+
+class MyQueue {
+    stack<int> s1;
+    stack<int> s2;
+public:
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        while(!s1.empty()){
+            s2.push(s1.top());
+            s1.pop();
+        }
+
+        s1.push(x);
+
+        while(!s2.empty()){
+            s1.push(s2.top());
+            s2.pop();
+        }
+    }
+    
+    int pop() {
+        int ans = s1.top();
+        s1.pop();
+        return ans;
+    }
+    
+    int peek() {
+        if(s1.empty()){
+            return -1;
+        }
+        return s1.top();
+    }
+    
+    bool empty() {
+        return s1.empty();
+    }
+};
+
+int main(){
+    MyQueue mq;
+    mq.push(1);
+    cout << mq.peek() << endl;
+    mq.push(2);
+    cout << mq.peek() << endl;
+    mq.pop();
+    cout << mq.peek() << endl;
+    mq.pop();
+    cout << mq.peek() << endl;
+    return 0;
+}
