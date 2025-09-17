606605d0d8d3d1e5b301bfac2262ba876af3d65a
manoj@ubuntu <manojkapure100@gmail.com>
2025-09-17 19:19:13 +0000

    Revision 2 sorting 2 question

--- Revision 17-09-2025 sorting 1-2.cpp
+++ Revision 17-09-2025 sorting 1-2.cpp
@@ -3,58 +3,49 @@
 
 using namespace std;
 
-void sortColors(vector<int> &arr)
-{
+void sortColors(vector<int> &arr){
     int low = 0;
     int mid = 0;
     int high = arr.size() - 1;
 
-    while (mid <= high)
-    {
-        switch (arr[mid])
-        {
-        case 0:
+    while(mid <= high){
+        if(arr[mid] == 0){
             swap(arr[mid], arr[low]);
+            mid++;
             low++;
+        } else if(arr[mid] == 1){
             mid++;
-            break;
-        case 1:
-            mid++;
-            break;
-        case 2:
-            swap(arr[mid], arr[high]);
+        } else {
+            swap(arr[high], arr[mid]);
             high--;
-            break;
-        default:
-            break;
         }
     }
 }
 
 void nextPermutation(vector<int> &arr){
-    int pointer = arr.size() - 1;
-    while(pointer > 0 && arr[pointer] < arr[pointer-1]){
+    int arrSize = arr.size();
+    int pointer = arrSize - 1;
+    while(arr[pointer] < arr[pointer-1]){
         pointer--;
     }
     if(pointer == 0){
         int start = 0;
-        int end = arr.size() - 1;
+        int end = arrSize - 1;
         while(start <= end){
             swap(arr[start], arr[end]);
             start++;
             end--;
         }
     } else {
-        int pivotElement = pointer - 1;
-        int lastElement = arr.size() - 1;
-        while(arr[pivotElement + 1] < arr[lastElement]){
-            lastElement--;
+        int pivotPointer = pointer - 1; //larger value
+        int lastPointer = arrSize - 1;
+        while(arr[pivotPointer + 1] < arr[lastPointer]){
+            lastPointer--;
         }
-        swap(arr[pivotElement], arr[lastElement]);
+        swap(arr[pivotPointer], arr[lastPointer]);
 
         int start = pointer;
-        int end = arr.size() - 1;
-
+        int end = arrSize - 1;
         while(start <= end){
             swap(arr[start], arr[end]);
             start++;
